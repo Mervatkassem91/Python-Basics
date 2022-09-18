12f9764b50b9569568934b7c40c948348c37247a
Mervatkassem91 <mervekm48@gmail.com>
2022-09-19 01:26:17 +0200

    study variables, operators,conditions with tasks

---
+++ 01-Variables & Operators/Code lecture/01-task.py
@@ -0,0 +1,88 @@
+>>> x = True
+>>> type(x)
+<class 'bool'>
+>>> 
+>>> y = 5
+>>> type(y)
+<class 'int'>
+>>> 
+>>> z = 2.5
+>>> type(z)
+<class 'float'>
+>>> 
+>>> s = 'hello world'
+>>> type(s)
+<class 'str'>
+>>> 
+>>> a = 10
+>>> type(a)
+<class 'int'>
+>>> a = a + 0.5
+a
+10.5
+type(a)
+<class 'float'>
+
+d = '5'
+type (d)
+<class 'str'>
+d = 5
+type (d)
+<class 'int'>
+
+L = [1,2,3,4,5]
+type(L)
+<class 'list'>
+
+t = (10,11,12,13,15,)
+type(t)
+<class 'tuple'>
+
+D = {1 :'mervat',2:'add',3:'phone'}
+type (D)
+<class 'dict'>
+
+X = 10
+X += 15
+X
+25
+
+X /= 5
+X
+5.0
+X *= 10
+X
+50.0
+
+X == 10
+False
+
+X != 5
+True
+
+X <= 50
+True
+
+2**3
+8
+
+11/3
+3.6666666666666665
+
+11//3
+3
+
+10 > 15
+False
+
+X = 10
+if X <= 15:
+    print('x is smaller than 15')
+
+    
+X is smaller than 15
+
+
+
+
+
---
+++ 01-Variables & Operators/Task/01.py
@@ -0,0 +1,83 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print ('Hello World')
+Hello World
+x = 5
+y2 = 10
+y_t = 10
+
+
+X = 10
+x = 5
+
+x
+5
+X
+10
+
+
+a , b = 4 , 6
+a
+4
+b
+6
+
+
+a,s,d = 1,2,3
+
+a
+1
+s
+2
+d
+3
+-------------
+SyntaxError: invalid syntax
+
+t = 'farida'
+y = "farida"
+u = '''farida'''
+t
+'farida'
+y
+'farida'
+u
+'farida'
+
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> c = 'mahmoud'
+>>> x = 5
+>>> print(type(c))
+<class 'str'>
+>>> print(type(x))
+<class 'int'>
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> x +=1
+>>> x *= 5
+>>> x
+30
+>>> 
+>>> x **= 2
+>>> x
+900
+>>> 900
+900
+>>> 
+>>> 
+>>> 
+============================================= RESTART: E:/django fullstack3/Dev/01-Python Basics/02-Conditons/con1.py =============================================
+welcome
+>>> 
+==== RESTART: E:/django fullstack3/Dev/01-Python Basics/02-Conditons/con1.py ===
+>>> 
+==== RESTART: E:/django fullstack3/Dev/01-Python Basics/02-Conditons/con1.py ===
+10
